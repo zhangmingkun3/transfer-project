b23c606044c2fbef7de337f98aebd15e7edd5469
zhangmingkun3 <zhangmingkun3@jd.com>
2020-08-22 16:52:15 +0800

    完善socket

--- src/main/java/com/lp/transfer/transferproject/service/SocketServer.java
+++ src/main/java/com/lp/transfer/transferproject/service/SocketServer.java
@@ -5,12 +5,23 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import static com.lp.transfer.transferproject.utils.CacheUtils.localCache;
@@ -30,114 +41,171 @@ public class SocketServer {
     @Value("${socket.port}")
     private Integer port;
 
-    private boolean started;
     private ServerSocket serverSocket;
     private ExecutorService executorService = Executors.newCachedThreadPool();
+    private static Selector selector;
 
+    /**
+     * 发送数据缓冲区
+     */
+    private static ByteBuffer rBuffer = ByteBuffer.allocate(1024);
 
     public static void main(String[] args){
 //        new SocketServer().start(8068);
     }
 
-
-    public void start(Integer port) {
-        log.info("port: {}, {}", this.port, port);
+    @PostConstruct
+    public void start() {
         try {
-            serverSocket = new ServerSocket(port == null ? this.port : port);
-            started = true;
-            log.info("Socket服务已启动，占用端口： {}", serverSocket.getLocalPort());
-        } catch (IOException e) {
-            log.error("端口冲突,异常信息：{}", e);
-            System.exit(0);
-        }
-
-        final int readArraySizePerRead = 4096;
-        while (started) {
-            try {
-                Socket socket = serverSocket.accept();
-                socket.setKeepAlive(true);
-
-
-                byte[] b = new byte[1024];
-                int len = 0;
-                String response = "";
-                while (true) {
-
-                    DataInputStream isr = new DataInputStream(socket.getInputStream());
-
-                    ArrayList<Byte> bytes = new ArrayList<>();
-
-                    byte[] tempchars = new byte[readArraySizePerRead];
-                    int charsReadCount = 0;
-                    while ((charsReadCount = isr.read(tempchars)) != -1) {
-                        for(int i = 0 ; i < charsReadCount ; i++){
-                            bytes.add (tempchars[i]);
-                        }
-                    }
-                    isr.close();
-
-                    byte[] out = toPrimitives(bytes.toArray(new Byte[0]));
-
-                    // 解析
-                    String asciiId = bytesToHexString(out);
-                    String id = AsciiStringToString(asciiId).replace("|","");
-                    System.out.println("AsciiId : " + asciiId  + " id ："+ id);
-
-                    List<Integer> totalList = new ArrayList<>();
-                    int sum = 0;
-                    for(int i = 16 ;i <out.length;i+=2){
-                        byte low = out[i];
-                        byte high = out[i+1];
-                        int x = merge(high,low);
-                        short low_short = unsignedByteToShort(low);
-                        short high_short = (short)(((high & 0x00FF) << 8));
-                        System.out.println( "low:" + low_short + "   high : " +  high_short + "   sum :" + x);
-                        sum+=1;
-                        totalList.add(x);
-                    }
-
-                    System.out.println("总数：sum = " + sum+"   arrayList Size:" + totalList.size() + "  arrayList:" + JSON.toJSONString(totalList) );
-
-                    len = isr.read(b);
-                    response = new String(b, 0, len);
-                    log.info("datadadata", response);
-
-
-
-                    if (null != localCache.getIfPresent(asciiId) && localCache.getIfPresent(asciiId).size() > 0){
-//                        Thread thread = shareMap.get(asciiId);
-
-                        List<String> list = localCache.get(asciiId);
-
-                        if (list.size() > 2999){
-                            // 触发计算  调用exe程序
-
-                            // 然后重新设置 Map 以及缓存
-                            localCache.put(asciiId,new ArrayList<>());
-                        }
-
-                        list.add("数据");
-                        localCache.put(asciiId,list);
-
-                    }else{
-
-                        COMMON_POOL.submit(() ->{
-                            List<String> list = new ArrayList<>();
-                            list.add("数据");
-                            localCache.put(asciiId,list);
-                        });
-
-                    }
-
+            // 打开通信信道
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            // 设置为非阻塞
+            serverSocketChannel.configureBlocking(false);
+            // 获取套接字
+            ServerSocket serverSocket = serverSocketChannel.socket();
+            // 绑定端口号
+            serverSocket.bind(new InetSocketAddress(port));
+            // 打开监听器
+            selector = Selector.open();
+            // 将通信信道注册到监听器
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            // 监听器会一直监听，如果客户端有请求就会进入相应的事件处理
+            while (true) {
+                // select方法会一直阻塞直到有相关事件发生或超时
+                selector.select();
+                // 监听到的事件
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                for (SelectionKey key : selectionKeys) {
+                    handle(key);
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+                // 清除处理过的事件
+                selectionKeys.clear();
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
+    private void handle(SelectionKey selectionKey) throws IOException {
+        ServerSocketChannel serverSocketChannel = null;
+        SocketChannel socketChannel = null;
+        int count = 0;
+        if (selectionKey.isAcceptable()) {
+            ////新连接请求，注册到选择器中   每有客户端连接，即注册通信信道为可读
+            serverSocketChannel = (ServerSocketChannel) selectionKey.channel();
+            socketChannel = serverSocketChannel.accept();
+            socketChannel.configureBlocking(false);
+            socketChannel.register(selector, SelectionKey.OP_READ);
+            sendInfo(socketChannel, "连接服务器成功!");
+            System.err.println("client IP :" + socketChannel.socket().getRemoteSocketAddress());
+        } else if (selectionKey.isReadable()) {
+            //连接可读请求，处理读业务逻辑
+            socketChannel = (SocketChannel) selectionKey.channel();
 
-    static byte[] toPrimitives(Byte[] oBytes) {
+            StringBuilder msg = new StringBuilder();
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            ArrayList<Byte> byteList = new ArrayList<>();
+
+            while(socketChannel.read(buffer) > 0) {
+                buffer.flip();
+                while(buffer.hasRemaining()){
+                    byte[] bytes = buffer.get(new byte[buffer.limit()]).array();
+                    msg.append(new String(buffer.get(new byte[buffer.limit()]).array()));
+                   for (byte b : bytes){
+                       byteList.add(b);
+                    }
+                }
+                buffer.clear();
+            }
+            System.err.println("收到客户端消息:"+msg);
+
+            byte[] out = toPrimitives(byteList.toArray(new Byte[0]));
+
+            // 解析
+            String asciiId = bytesToHexString(out);
+            String id = AsciiStringToString(asciiId).replace("|","");
+            System.out.println("AsciiId : " + asciiId  + " id ："+ id);
+
+            List<Integer> totalList = new ArrayList<>();
+            int sum = 0;
+            for(int i = 16 ;i <out.length;i+=2){
+                byte low = out[i];
+                byte high = out[i+1];
+                int x = merge(high,low);
+                short low_short = unsignedByteToShort(low);
+                short high_short = (short)(((high & 0x00FF) << 8));
+                System.out.println( "low:" + low_short + "   high : " +  high_short + "   sum :" + x);
+                sum+=1;
+                totalList.add(x);
+            }
+
+            System.out.println("总数：sum = " + sum+"   arrayList Size:" + totalList.size() + "  arrayList:" + JSON.toJSONString(totalList) );
+
+
+            if (null != localCache.getIfPresent(asciiId) && localCache.getIfPresent(asciiId).size() > 0){
+                List<String> list = localCache.get(asciiId);
+                if (list.size() > 2999){
+                    // 触发计算  调用exe程序
+
+                    // 然后重新设置 Map 以及缓存
+                    localCache.put(asciiId,new ArrayList<>());
+                }
+
+                list.add("数据");
+                localCache.put(asciiId,list);
+
+            }else{
+
+                COMMON_POOL.submit(() ->{
+                    List<String> list = new ArrayList<>();
+                    list.add("数据");
+                    localCache.put(asciiId,list);
+                });
+
+            }
+
+            socketChannel.close();
+        }
+    }
+
+    private void sendInfo(SocketChannel clientChannel, String msg){
+        // 向客户端发送连接成功信息
+        try {
+            clientChannel.write(ByteBuffer.wrap(msg.getBytes()));
+        } catch (IOException e) {
+            log.info("向请求端发送回执消息失败");
+            e.printStackTrace();
+        }
+    }
+
+        private static String toHex(byte[] buffer) {
+        StringBuffer sb = new StringBuffer(buffer.length * 2);
+        for (int i = 0; i < buffer.length; i++) {
+            sb.append(Character.forDigit((buffer[i] & 240) >> 4, 16));
+            sb.append(Character.forDigit(buffer[i] & 15, 16));
+        }
+        return sb.toString();
+    }
+
+
+
+    private void handleAccept(SelectionKey selectionKey) throws Exception {
+        // 返回创建此键的通道，接受客户端建立连接的请求，并返回 SocketChannel 对象
+        ServerSocketChannel ServerSocketChannel = (ServerSocketChannel) selectionKey.channel();
+        SocketChannel clientChannel = ServerSocketChannel.accept();
+        // 非阻塞式
+        clientChannel.configureBlocking(false);
+        // 注册到selector
+        clientChannel.register(selectionKey.selector(), SelectionKey.OP_READ);
+
+//        sendInfo(clientChannel, "连接服务器成功!");
+        System.err.println("client IP :" + clientChannel.socket().getRemoteSocketAddress());
+    }
+
+
+
+
+        static byte[] toPrimitives(Byte[] oBytes) {
         byte[] bytes = new byte[oBytes.length];
 
         for (int i = 0; i < oBytes.length; i++) {
